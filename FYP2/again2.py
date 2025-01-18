fc5d01be0b048d5a73493c1f5f61483d0a4e7b76
Trichor <140474490+Trichor@users.noreply.github.com>
2025-01-18 22:07:47 +0800

    Added move movement and left click inputs, fied camera invert and fixed a bug

--- FYP2/again2.py
+++ FYP2/again2.py
@@ -3,7 +3,10 @@ import cv2
 import mediapipe as mp
 import numpy as np
 from tensorflow.keras.models import load_model
+import win32api
+import win32con
 import pyautogui
+import math
 
 # this 2 libraries for the on screen keyboard and more encoding support accross various devices
 import os
@@ -62,6 +65,66 @@ def open_onscreen_keyboard():
     except Exception as e:
         print(f"Failed to open on-screen keyboard: {e}")
 
+# Gesture-to-key mapping
+"""gesture_to_key = {
+    'call': 'c',            # Clutch
+    'dislike': 'q',             # Shift down
+    'fist': 'space',            # E-brake
+    'like': 'e',                # Shift up
+    'ok': 'k',                  # Custom key
+    'one': 'w',                 # Acceleratee
+    'peace': 'a',               # Steer left
+    'peace_inverted': 'd',      # Steer right
+    'rock': 'r',                # ANNA Activation
+    'stop': 's',                # Decelerate/reverse
+    'stop_inverted': 'g',       # Custom key
+    'three': '3'                # Custom key
+}"""
+
+screen_width, screen_height = pyautogui.size()
+cursor_speed = 5
+position_displacement = 1.5
+previous_base_coord = [0, 0]
+is_left_click_held = False
+
+# Cursor D8 Movement function
+def dpadCursor(index_x, index_y):
+    # Create deadzone
+    screen_center_vector = [screen_width / 2, screen_height / 2]
+    
+    deadzone_width = screen_width / 6
+    deadzone_height = screen_height / 6
+    deadzone_min_vector = [screen_center_vector[0] - (deadzone_width / 2), screen_center_vector[1] - (deadzone_height / 2)]
+    deadzone_max_vector = [screen_center_vector[0] + (deadzone_width / 2), screen_center_vector[1] + (deadzone_height / 2)]
+    
+    dist_from_center = calculate_distance(screen_center_vector[0], screen_center_vector[1], index_x, index_y)
+    current_speed = int(cursor_speed * (dist_from_center/100))
+    #print(str(dist_from_center))
+    current_cursor_x, current_cursor_y = pyautogui.position()
+    
+    if not (deadzone_min_vector[0] < index_x < deadzone_max_vector[0] and deadzone_min_vector[1] < index_y < deadzone_max_vector[1]):
+        #control the movement of the cursor, similar to a d-pad
+        if index_x < deadzone_min_vector[0] and index_y < deadzone_min_vector[1]:                             #top left position
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -current_speed, -current_speed, 0, 0)
+        if deadzone_min_vector[0] < index_x < deadzone_max_vector[0] and index_y < deadzone_min_vector[1]:    #top middle position
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, -current_speed, 0, 0)
+        if deadzone_max_vector[0] < index_x  and index_y < deadzone_min_vector[1]:                            #top right position
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, current_speed, -current_speed, 0, 0)
+        if index_x < deadzone_min_vector[0]  and deadzone_min_vector[1] < index_y < deadzone_max_vector[1]:   #middle left position
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -current_speed, 0, 0, 0)
+        if deadzone_max_vector[0] < index_x  and deadzone_min_vector[1] < index_y < deadzone_max_vector[1]:   #middle right position
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, current_speed, 0, 0, 0)
+        if index_x < deadzone_min_vector[0] and deadzone_max_vector[1] < index_y:                             #bottom left position
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -current_speed, current_speed, 0, 0)
+        if deadzone_min_vector[0] < index_x < deadzone_max_vector[0] and deadzone_max_vector[1] < index_y:    #bottom middle position
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, current_speed, 0, 0)
+        if deadzone_max_vector[0] < index_x  and deadzone_max_vector[1] < index_y:                            #bottom right position
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, current_speed, current_speed, 0, 0)
+            
+def calculate_distance(x1, y1, x2, y2):
+    """Calculate the Euclidean distance between two points."""
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
 
 # Open webcam
 cap = cv2.VideoCapture(0)
@@ -71,6 +134,7 @@ while cap.isOpened():
     if not ret:
         break
 
+    frame = cv2.flip(frame, 1)
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
 
@@ -95,8 +159,34 @@ while cap.isOpened():
             if gesture_name in gesture_to_key:
                 action = gesture_to_key[gesture_name]
                 if action == 'open_keyboard':
+                    if is_left_click_held:
+                        pyautogui.mouseUp()
+                        is_left_click_held = False
                     open_onscreen_keyboard()  # Call the function to open the on-screen keyboard
+                elif action == 'mouse_movement':
+                    if is_left_click_held:
+                        pyautogui.mouseUp()
+                        is_left_click_held = False
+                    # Get fingertip coordinates and move the mouse
+                    index_base = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
+                    
+                    # Scale coordinates to the screen
+                    index_x = int(index_base.x * screen_width)
+                    index_y = int(index_base.y * screen_height)
+                    
+                    #Functions to move the cursor (Do not run both functions at once!)
+                    dpadCursor(index_x, index_y) #Run D8 movement function
+                    #positionCursor(index_x, index_y, index_base, previous_base_coord) #Run hand position cursor
+                    
+                    previous_base_coord = [index_base.x, index_base.y]
+                elif action == 'left_click':
+                    if not is_left_click_held:
+                        pyautogui.mouseDown()
+                        is_left_click_held = True
                 else:
+                    if is_left_click_held:
+                        pyautogui.mouseUp()
+                        is_left_click_held = False
                     pyautogui.press(action)  # Simulate key press
 
             # Display the gesture name on the frame
